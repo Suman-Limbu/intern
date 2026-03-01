d7ee3d1d2f9ed83d1ab80c3210de6575ef40195c
Suman-Limbu <sumanscoopie123@gmail.com>
2026-03-01 12:53:46 +0545

    a

--- src/Components/FilterSection.jsx
+++ src/Components/FilterSection.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 
-const FilterSection = ({ search, setSearch, categories,category, setCategory }) => {
+const FilterSection = ({
+  search,
+  setSearch,
+  categories,
+  category,
+  setCategory,
+  price,
+  setPrice,
+  sort,
+  setSort,
+  showFilter,
+  setShowFilter,
+  suggestion,
+}) => {
+  console.log(suggestion);
   return (
     <>
-      <div className="bg-blue-200 shadow-md rounded-md py-3 px-2 w-[270px] h-[250px] gap-3">
+      <div className="bg-blue-200 shadow-md rounded-md py-3 px-2 w-[270px] h-[350px] gap-3">
+        <button
+          className="cursor-pointer"
+          onClick={() => setShowFilter(!showFilter)}
+        >
+          x
+        </button>
         <h1 className="text-orange-400 font-bold text-2xl text-center ">
           Filters
         </h1>
@@ -15,6 +35,13 @@ const FilterSection = ({ search, setSearch, categories,category, setCategory }) 
             value={search}
             onChange={(e) => setSearch(e.target.value)}
           />
+          {suggestion && (
+            <ul>
+              {suggestion.map((s) => (
+                <li onClick={()=>setSearch(s)}>{s}</li>
+              ))}
+            </ul>
+          )}
         </div>
         <div>
           <p className=" font-semibold text-red-600 ">Category</p>
@@ -23,8 +50,30 @@ const FilterSection = ({ search, setSearch, categories,category, setCategory }) 
             onChange={(e) => setCategory(e.target.value)}
           >
             {categories.map((itm, idx) => (
-              <option key={idx} value={itm}> {itm}</option>
+              <option key={idx} value={itm}>
+                {itm}
+              </option>
             ))}
+          </select>
+        </div>
+        <div>
+          <h1 className=" font-semibold text-red-600 ">Price</h1>
+          <p>
+            Price Range: ${price[0]}-${price[1]}
+          </p>
+          <input
+            placeholder="enter price"
+            type="range"
+            min={0}
+            max={5000}
+            value={price[1]}
+            onChange={(e) => setPrice([price[0], Number(e.target.value)])}
+          />
+          <h1 className=" font-semibold text-red-600 ">Sort</h1>
+          <select value={sort} onChange={(e) => setSort(e.target.value)}>
+            <option value={""}>None</option>
+            <option value={"low"}>Low</option>
+            <option value={"high"}>High</option>
           </select>
         </div>
       </div>
--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -6,45 +6,93 @@ import FilterSection from "../Components/FilterSection";
 const Products = () => {
   const [products, setProducts] = useState([]);
   const [search, setSearch] = useState("");
-  const [category, setCategory] = useState("All");
-  console.log(products);
+  const [category, setCategory] = useState("all");
+  const [price, setPrice] = useState([0, 5000]);
+  const [sort, setSort] = useState("");
+  const [showFilter, setShowFilter] = useState(false);
+
   useEffect(() => {
-    const fetchProducts = async () => {
-      const res = await axios.get("https://fakestoreapi.com/products");
-      setProducts(res.data);
-    };
-    fetchProducts();
+    try {
+      const fetchProducts = async () => {
+        const res = await axios.get("https://fakestoreapi.com/products");
+        setProducts(res.data);
+      };
+      fetchProducts();
+    } catch (error) {
+      console.log(error);
+    }
   }, []);
 
   const categories = useMemo(() => {
     return ["all", ...new Set(products.map((p) => p.category))];
   }, [products]);
+  const suggestion = useMemo(() => {
+if(search.length>0){
+      return products
+      .filter((itm) => itm.title.toLowerCase().includes(search.toLowerCase()))
+      .map((itm) => itm.title)
+      .slice(0, 5);
+}
+  }, [products,search]);
 
   const filterProducts = useMemo(() => {
     let result = [...products];
-    if (search) {
+    if (search.length > 0) {
       result = result.filter((itm) =>
         itm.title.toLowerCase().includes(search.toLowerCase()),
       );
     }
 
-    if (category !== "All") {
+    if (category !== "all") {
       result = result.filter((itm) => itm.category === category);
     }
+
+    if (price.length > 0) {
+      result = result.filter(
+        (itm) => itm.price >= price[0] && itm.price <= price[1],
+      );
+    }
+    if (sort.length > 0) {
+      if (sort == "low") {
+        result = result.sort((a, b) => a.price - b.price);
+      }
+      if (sort == "high") {
+        result = result.sort((a, b) => b.price - a.price);
+      }
+    }
     return result;
-  }, [products, search, category]);
+  }, [products, search, category, price, sort]);
   return (
     <>
+      {!showFilter && (
+        <button
+          onClick={() => setShowFilter(!showFilter)}
+          className="bg-blue-200 rounded cursor-pointer"
+        >
+          Filter
+        </button>
+      )}
+      <div>
+        {showFilter && (
+          <div>
+            <FilterSection
+              search={search}
+              setSearch={setSearch}
+              categories={categories}
+              category={category}
+              setCategory={setCategory}
+              price={price}
+              setPrice={setPrice}
+              sort={sort}
+              setSort={setSort}
+              showFilter={showFilter}
+              setShowFilter={setShowFilter}
+              suggestion={suggestion}
+            />
+          </div>
+        )}
+      </div>
       <div className=" max-w-7xl mx-auto grid grid-cols-4 gap-6 mt-6">
-        <div>
-          <FilterSection
-            search={search}
-            setSearch={setSearch}
-            categories={categories}
-            category={category}
-            setCategory={setCategory}
-          />
-        </div>
         {filterProducts.map((itm) => (
           <div key={itm.id}>
             <ProductCard
