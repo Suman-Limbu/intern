218ff58f33cf5515ada975957f6dd3bf38c6ebd8
Suman-Limbu <sumanscoopie123@gmail.com>
2026-03-07 23:04:48 +0545

    a

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,9 +1,21 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
 
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-  const cart = "helllo";
-  return <CartContext.Provider value={cart}>{children}</CartContext.Provider>;
+  const reducer = (state, action) => {
+    if (action.type === "add") {
+      return [...state, action.payload];
+    }
+    return state;
+  };
+  const [cart, dispatch] = useReducer(reducer, []);
+  console.log(cart);
+  const addToCart = (product) => {
+    dispatch({ type: "add", payload: product });
+  };
+  return (
+    <CartContext.Provider value={{addToCart}}>{children}</CartContext.Provider>
+  );
 };
 export const useCart = () => useContext(CartContext);
