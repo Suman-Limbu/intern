1bc38e8b33532798cd597a3bd50711c77b5f8ef2
Suman-Limbu <sumanscoopie123@gmail.com>
2026-03-07 23:04:43 +0545

    cart

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,18 +1,23 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useCart } from "../context/CartContext";
 
 const ProductCard = ({ id, title, brand, category, price, image }) => {
-  const navigate=useNavigate();
-  const [itemCart,setItemCard]=useState([]);
+  const { addToCart } = useCart();
+  const handleAdd = () => {
+    addToCart({ id, title, brand, category, price, image });
+  };
   return (
     <>
       <div className="bg-yellow-200 w-[300px] h-[300px] rounded-lg shadow-lg py-3 px-5">
-        <img src={image} className="w-[100px]"/>
+        <img src={image} className="w-[100px]" />
         <h1>{title}</h1>
         <span>{price}</span>
         <span>{brand}</span>
         <span>{category}</span>
-        <button className="px-3 py-2 bg-red-500 rounded" onClick={()=>navigate("/cart")}>Add to cart</button>
+        <button className="px-3 py-2 bg-red-500 rounded" onClick={handleAdd}>
+          Add to cart
+        </button>
       </div>
     </>
   );
