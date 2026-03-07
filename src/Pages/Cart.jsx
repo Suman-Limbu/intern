a9d043606d36c62487d493c79910bb60f6cbe8cd
Suman-Limbu <sumanscoopie123@gmail.com>
2026-03-07 18:35:32 +0545

    a

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,26 +1,24 @@
-import React, { useReducer } from 'react'
-import { useCart } from '../context/CartContext'
+import React, { useReducer } from "react";
+import { useCart } from "../context/CartContext";
 
 const Cart = () => {
-    const reducer=(state,action)=>{
-if(action.type==="Inc"){
-return state+1
-}
-if(action.type==="Dec"){
-return state-1
-}
-
-  }
-  const [count,dispatch]=useReducer(reducer,0);
+  const reducer = (count, action) => {
+    if (action.type === "Inc") {
+      return count + 1;
+    }
+    if (action.type === "Dec") {
+      return count - 1;
+    }
+  };
+  const [count, dispatch] = useReducer(reducer, 0);
 const cart=useCart();
   return (
-  <>
-  {count}
-  {cart}
-  <button onClick={()=>dispatch({type:"Inc"})}>Inc</button>
-  <button onClick={()=>dispatch({type:"Dec"})}>Dec</button>
-  </>
-  )
-}
+    <>{count}
+    {cart}
+      <button onClick={() => dispatch({ type: "Inc" })}>Inc</button>
+      <button onClick={() => dispatch({ type: "Dec" })}>Dec</button>
+    </>
+  );
+};
 
 export default Cart;
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,12 +1,9 @@
 import { createContext, useContext } from "react";
 
-export const CartContext=createContext();
+export const CartContext = createContext();
 
-export const CartProvider=({children})=>{
-   
-    return (<CartContext.Provider >
-    {children}
-    </CartContext.Provider>)
-} 
-
-export const useCart=()=>useContext(CartContext);
+export const CartProvider = ({ children }) => {
+  const cart = "helllo";
+  return <CartContext.Provider value={cart}>{children}</CartContext.Provider>;
+};
+export const useCart = () => useContext(CartContext);
