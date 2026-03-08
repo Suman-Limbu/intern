35a395bd28afdb44d72cb4de35d3b3e596a11a10
Suman-Limbu <sumanscoopie123@gmail.com>
2026-03-08 14:47:39 +0545

    a

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,21 @@ import ProductCard from "./Components/ProductCard";
 import Products from "./Pages/Products";
 import Cart from "./Pages/Cart";
 import Navbar from "./Components/Navbar";
-
+import Admin from "./Pages/Admin";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 
 const App = () => {
   return (
     <>
-
-      <Navbar/>
+      <Navbar />
       <Routes>
         <Route path={"/"} element={<Products />} />
         <Route path={"/cart"} element={<Cart />} />
+        <Route path={"/admin"} element={<Admin />} />
+        <Route path={"/login"} element={<Login />} />
+        <Route path={"/Register"} element={<Register />} />
       </Routes>
-  
     </>
   );
 };
