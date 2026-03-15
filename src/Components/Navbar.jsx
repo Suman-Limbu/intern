23042b660e0689a7608ecabca60dcdc5b84f42b3
Suman-Limbu <sumanscoopie123@gmail.com>
2026-03-15 12:10:47 +0545

    a

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,33 +1,33 @@
 import React from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { useCart } from "../context/CartContext";
-import Login from "../Pages/Auth/Login";
+
+import { CarTaxiFrontIcon } from "lucide-react";
 
 const Navbar = () => {
   const { cart } = useCart();
   const navigate = useNavigate();
   return (
-    <div className="w-full bg-blue-200 h-[60px] flex">
-    <nav>
-      <NavLink to="/">
-      Home
-      </NavLink>
-       <NavLink to="/products">
-      Products
-      </NavLink>
-      
-       <NavLink to="/cart">
-      Cart
-      </NavLink>
-       <NavLink to="/contact">
-      Contact
-      </NavLink>
-    </nav>
-    
-      <div className="bg-red-500 rounded-full w-[20px] h-[20px]">
-        {cart.length}
+    <div className="w-full bg-slate-600 h-[60px] flex items-center justify-between px-6">
+      <nav className="w-7xl flex gap-6 font-semibold text-lg cursor-pointer">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/products">Products</NavLink>
+
+        <NavLink to="/cart">Cart</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+      </nav>
+
+      <div className="flex gap-2 items-center">
+        <div className="">
+         <CarTaxiFrontIcon/>
+
+        </div>
+        <span className="bg-red-500 rounded-full w-[20px] h-[20px]">
+          {cart.length}
+         
+        </span>
+        <button onClick={() => navigate("/login")}>Login</button>
       </div>
-      <button onClick={() => navigate("/login")}>Login</button>
     </div>
   );
 };
